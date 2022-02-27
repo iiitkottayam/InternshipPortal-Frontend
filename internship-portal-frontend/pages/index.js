3794b27c0112210be9a4e6c8749dd48333937c2b
Shashank Srivastava <54381338+shashank1503-cipher@users.noreply.github.com>
2022-02-27 19:53:39 +0530

    Added Color Mode

--- internship-portal-frontend/pages/index.js
+++ internship-portal-frontend/pages/index.js
@@ -1,34 +1,45 @@
-import { Flex, Spinner, Text } from "@chakra-ui/react";
+import { Button, Flex, Spinner, Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const color = useColorModeValue('blue.500', 'cyan')
   return (
-    <Flex
-      justifyContent={"center"}
-      alignItems={"center"}
-      height={"100vh"}
-      direction={"column"}
-    >
+    <>
       <Head>
         <title>Internship Portal</title>
         <meta name="description" content="IIIT Kottayam Internship Portal" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Text fontSize={"xl"}>IIIT Kottayam's Inhouse</Text>
-      <Text
-        color={"cyan"}
-        fontSize="6xl"
-        fontWeight="extrabold"
-        fontFamily={"body"}
-      >
-        Internship Portal
-      </Text>
-      <Flex>
-        <Text fontSize={"xl"}>Coming Soon</Text>
-        <Spinner ml={5} mt={1}/>
+      <Flex direction={"column"}>
+        <Flex justifyContent={"flex-end"} m={10}>
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"} Mode
+          </Button>
+        </Flex>
+        <Flex
+          justifyContent={"center"}
+          alignItems={"center"}
+          direction={"column"}
+          height={"80vh"}
+        >
+          <Text fontSize={"xl"}>IIIT Kottayam's Inhouse</Text>
+          <Text
+            color={color}
+            fontSize="6xl"
+            fontWeight="extrabold"
+            fontFamily={"body"}
+          >
+            Internship Portal
+          </Text>
+          <Flex>
+            <Text fontSize={"xl"}>Coming Soon</Text>
+            <Spinner ml={5} mt={1} />
+          </Flex>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
